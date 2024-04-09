d2cb79102165959c17d69b1446df00dbfb56400d
Eunice Meng <eunicemenggg@gmail.com>
2024-04-10 02:30:29 +0800

    Implement today's weather section

--- src/features/todaysWeather.jsx
+++ src/features/todaysWeather.jsx
@@ -1,17 +1,31 @@
+import React from "react";
 import "./todaysWeather.css";
+import { useTodaysWeatherStore } from "../contexts/todaysWeatherStore";
 
 function TodaysWeather() {
+	const weather = useTodaysWeatherStore((state) => state.weather);
+
 	return (
 		<div className="todays-weather">
 			<p>Today's weather</p>
-			<p className="current-temperature">26°C</p>
-			<p className="highest-lowest-temp">H: 29° L: 26°</p>
-			<div className="more-info">
-				<div className="state-country">Johor, MY</div>
-				<div className="date-time">08-04-2024 09:41am</div>
-				<div className="humidity">Humidity: 58%</div>
-				<div className="weather-conditions">Clouds</div>
-			</div>
+			{Object.keys(weather).length === 0 ? (
+				<div id="no-weather-to-display">
+					Search for a city to view the weather!
+				</div>
+			) : (
+				<React.Fragment>
+					<p className="current-temperature">{weather.currentTemp}°</p>
+					<p className="highest-lowest-temp">
+						H: {weather.highestTemp}° L: {weather.lowestTemp}°
+					</p>
+					<div className="more-info">
+						<div className="state-country">{weather.displayText}</div>
+						<div className="date-time">{weather.timestamp}</div>
+						<div className="humidity">Humidity: {weather.humidity}%</div>
+						<div className="weather-conditions">{weather.description}</div>
+					</div>
+				</React.Fragment>
+			)}
 		</div>
 	);
 }
